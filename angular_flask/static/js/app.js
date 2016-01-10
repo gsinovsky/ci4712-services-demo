eaed6ae73cabf39245e6148dc20c900198d6c3c4
Gustavo Siñovsky <gsinovsky@talpor.com>
2016-01-10 12:34:53 -0430

    Removes useless routes

--- angular_flask/static/js/app.js
+++ angular_flask/static/js/app.js
@@ -7,29 +7,7 @@ angular.module('AngularFlask', ['angularFlaskServices'])
 		.when('/', {
 			templateUrl: 'static/partials/landing.html',
 			controller: IndexController
-		})
-		.when('/about', {
-			templateUrl: 'static/partials/about.html',
-			controller: AboutController
-		})
-		.when('/post', {
-			templateUrl: 'static/partials/post-list.html',
-			controller: PostListController
-		})
-		.when('/post/:postId', {
-			templateUrl: '/static/partials/post-detail.html',
-			controller: PostDetailController
-		})
-		/* Create a "/blog" route that takes the user to the same place as "/post" */
-		.when('/blog', {
-			templateUrl: 'static/partials/post-list.html',
-			controller: PostListController
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
-		;
-
+		});
 		$locationProvider.html5Mode(true);
 	}])
 ;
